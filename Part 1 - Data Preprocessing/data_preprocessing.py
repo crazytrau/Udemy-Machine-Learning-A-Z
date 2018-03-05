e820d606e3359b24d405121633986c2dba2663bc
crazytrau <htphuocdl@gmail.com>
2018-03-05 11:29:01 +0700

    simple linear regression

--- Part 1 - Data Preprocessing/data_preprocessing.py
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -12,7 +12,28 @@ import pandas as pd
 #import dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:,0:3].values
-Y = dataset.iloc[:,3].values
+y = dataset.iloc[:,3].values
+
+#splitting dataset to training set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+## Feature scaling
+#from sklearn.preprocessing import StandardScaler
+#sc_X = StandardScaler()
+#X_train = sc_X.fit_transform(X_train)
+#X_test = sc_X.transform(X_test)
+
+#=======================================================================================================
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#import dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:,0:3].values
+y = dataset.iloc[:,3].values
 
 ## take care of missing values
 #from sklearn.preprocessing import Imputer
